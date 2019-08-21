87de2e54860f001d4d6fe29e95a612200dc9e08b
Xiaochuan Ma <mxcins@gmail.com>
2019-08-21 13:25:24 +0800

    update

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const request = require('request').defaults({ jar: true });
 
-io.use((socket, next) => {
+const chat = io.of('/socket/chat');
+
+chat.use((socket, next) => {
+  if (!socket.handshake.headers.cookie) {
+    return next(new Error('Authorization Error'));
+  }
   const jar = request.jar();
-  console.log(socket.handshake.headers.cookie);
   const cookie = request.cookie(socket.handshake.headers.cookie);
   jar.setCookie(cookie, 'http://127.0.0.1:3000');
   request({
@@ -20,16 +24,16 @@ io.use((socket, next) => {
   });
 })
 
-io
-  .of('/socket/chat')
-  .on('connection', socket => {
-    socket.on('chat message', d => {
-      console.log('123', d);
-    })
-    socket.on('error', error => {
-      console.log('on error', error);
-    })
+
+chat.on('connection', socket => {
+  socket.on('chat message', msg => {
+    console.log('on chat message', msg);
+    chat.emit('chat message', msg);
   })
+  socket.on('error', error => {
+    console.log('on error', error);
+  })
+})
 
 http.listen(3001, () => {
   console.log('listening on *:3001');
