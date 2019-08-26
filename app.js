d99c9c783f8e9824fcf0a6f4869362b0d1742501
Xiaochuan Ma <mxcins@gmail.com>
2019-08-26 18:53:56 +0800

    update authenticate

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const request = require('request');
+const request = require('request-promise-native');
 
-const chat = io.of('/socket/chat');
+const chat = io.of('/chat');
 
-chat.use((socket, next) => {
-  if (!socket.handshake.headers.cookie) {
-    return next(new Error('Authorization Error'));
-  }
-  request({
-    method: 'POST',
-    uri: 'http://127.0.0.1:3000/api/check.json',
-    headers: {
-      Cookie: socket.handshake.headers.cookie,
-    },
-  }, (err, response, body) => {
-    if (response && response.statusCode === 204) {
-      return next();
+chat.use(async (socket, next) => {
+  try {
+    if (socket.handshake.headers.cookie) {
+      const resp = await request({
+        method: 'POST',
+        uri: 'http://127.0.0.1:3000/api/authenticate.json',
+        headers: {
+          Cookie: socket.handshake.headers.cookie,
+        },
+      });
+      console.log('resp', resp);
+      next();
     }
-    return next(new Error('Authorization Error'));
-  });
+  } catch (error) {
+    console.log('error', error);
+    // (err, response, body) => {
+    //   if (response && response.statusCode === 204) {
+    //     return next();
+    //   }
+    next(new Error('Authorization Error'));
+    // }
+  }
 })
 
 
 chat.on('connection', socket => {
+  console.log('some one connected');
   socket.on('chat message', msg => {
     chat.emit('chat message', msg);
   })
